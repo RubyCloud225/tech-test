3da6c4a3ffa7c5a48df5921928ed0a400d9540c9
Catherine Earl <116440382+RubyCloud225@users.noreply.github.com>
2026-07-19 23:10:33 +0100

    Implement ScreenResultPrinter::printResults
    Prints TradeID : Result : Error, TradeID : Result, or TradeID : Error
    depending on which of the optional result/error fields are present.

--- cpp/RiskSystem/ScreenResultPrinter.cpp
+++ cpp/RiskSystem/ScreenResultPrinter.cpp
@@ -1,7 +1,7 @@
 #include "ScreenResultPrinter.h"
 #include <iostream>
 
-void ScreenResultPrinter::printResults(ScalarResults& results) {
+void ScreenResultPrinter::printResults(const ScalarResults& results) {
     for (const auto& result : results) {
         // Write code here to print out the results such that we have:
         // TradeID : Result : Error
@@ -9,5 +9,13 @@ void ScreenResultPrinter::printResults(ScalarResults& results) {
         // TradeID : Error
         // If there is no error the output should be:
         // TradeID : Result
+        std::cout << result.getTradeId();
+        if (result.getResult().has_value()) {
+            std::cout << " : " << result.getResult().value();
+        }
+        if (result.getError().has_value()) {
+            std::cout << " : " << result.getError().value();
+        }
+        std::cout << "\n";
     }
 }
